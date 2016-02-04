b2631d3aac3315a26f7ae108eb16f8b6707bb83e
helgir <helgir87@gmail.com>
2016-02-04 02:56:26 +0000

    Added room.html, user is now welcomed with message after logging in

--- client/js/main.js
+++ client/js/main.js
@@ -16,18 +16,32 @@ function($scope, $http){
 
 	var socket = io.connect("http://localhost:8080");
 
+
+
 	socket.on("roomlist", function(data) {
+		$scope.$apply(function() {
+		$scope.rooms = data;
 		console.log(data);
+		});
 	});
+
 	$scope.nick = "";
+	$scope.loggedIn = false;
+	$scope.rooms = [];
+
 		$scope.login = function(){
 			socket.emit("adduser", $scope.nick, function(available){
 		    if (available){
-
+		    	$scope.loggedIn = true;
 		    	socket.emit("rooms");
 		    }
 	});
 		};
 
+}]);
+
+angular.module("ChatApp").controller("RoomCtrl", 
+["$scope", "$http",
+function($scope, $http){
 }]);
 

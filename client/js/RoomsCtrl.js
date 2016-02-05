4a409175c80c338fa948c86c127638f22dcff4d4
helgir <helgir87@gmail.com>
2016-02-05 19:32:15 +0000

    User can now create a lobby

--- client/js/RoomsCtrl.js
+++ client/js/RoomsCtrl.js
@@ -1,12 +1,26 @@
 angular.module("ChatApp").controller("RoomsCtrl", 
-["$scope", "$http", "$routeParams", "socket", "$rootScope",
+["$scope", "$http", "$routeParams", "$location", "socket", "$rootScope",
 
-	function($scope, $http, $routeParams, socket, $rootScope){
+	function($scope, $http, $routeParams, $location, socket, $rootScope){
 
-		$scope.lobbyId = $routeParams.lobbyId;
 		$scope.nickId = $routeParams.nickId;
-		socket.emit("rooms");
+
+		
 		$scope.rooms = [];
+		$scope.lobbyId = '';
+		console.log("123131");
+		$scope.newRoom = function() {
+		
+			socket.emit('joinroom', { room:$scope.lobbyId }, function (success, reason) {
+
+				if (success) {
+					
+					$location.path('/rooms/' + $scope.nickId + '/' + $scope.lobbyId);
+				} 
+			});
+		};
+		socket.emit("rooms");
+
 
 		socket.on("roomlist", function(data) {
 			$scope.rooms = data;

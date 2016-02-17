b32dff459ec9585342ebaa0dc051b6ab59310dac
Helgi Rúnar Einarsson <HelgiR@HelgiR.local>
2016-02-17 13:13:43 +0000

    Conflict

--- client/js/RoomCtrl.js
+++ client/js/RoomCtrl.js
@@ -1,116 +1,117 @@
-angular.module("ChatApp").controller("RoomCtrl", ["$scope", "$http", "$routeParams", "$location", "socket", "$rootScope",
+angular.module("ChatApp").controller("RoomCtrl", 
+["$scope", "$http", "$routeParams", "$location", "socket", "$rootScope", 
+
+	
+	function($scope, $http, $routeParams, $location, socket, $rootScope) {
+
+		$scope.roomId = $routeParams.roomId;
+		$scope.nickId = $routeParams.nickId;
+		$scope.nicks = [];
+		$scope.submitMessage = '';
+		$scope.messages = [];
+		$scope.topic = '';
+		$scope.userSelected = false;
+		$scope.nickSelected = '';
+		$scope.pmMessages = [];
+		$scope.pmSubmitMessage = '';
 
 
-    function($scope, $http, $routeParams, $location, socket, $rootScope) {
+		socket.emit('joinroom', { room: $scope.roomId }, function (success, reason) {
 
-        $scope.roomId = $routeParams.roomId;
-        $scope.nickId = $routeParams.nickId;
-        $scope.nicks = [];
-        $scope.submitMessage = '';
-        $scope.messages = [];
-        $scope.topic = '';
-        $scope.userSelected = false;
-        $scope.nickSelected = '';
-        $scope.pmMessages = [];
-        $scope.pmSubmitMessage = '';
+		if (success) {
+			//TODO
 
+			//Checks if it was succsessful
+			// Could be password protected...
+			
+		}
+	});
 
-        socket.emit('joinroom', {
-            room: $scope.roomId
-        }, function(success, reason) {
+			
+				socket.on('updateusers', function (roomId, nicksId, ops) {
 
-            if (success) {
-                console.log(JSON.stringify(reason));
+					
+					if($scope.roomId === roomId) {
 
-            }
-        });
+						$scope.nicks = nicksId;
+					}
+				});
 
 
-        socket.on('updateusers', function(roomId, nicksId, ops) {
+				$scope.sendMSG = function() {
+					if($scope.submitMessage === '') {
+						//skip empty text
+					}
+					else {
+						socket.emit('sendmsg', { roomName: $scope.roomId, msg: $scope.submitMessage });
+					}
+					
 
+					$scope.submitMessage = '';
+					var scrollToBottom = $('#content');
+						scrollToBottom.animate({ scrollTop: scrollToBottom.prop('scrollHeight') }, 1000);
+				};
 
-            if ($scope.roomId === roomId) {
+				$scope.sendPmMSG = function () {
 
-                $scope.nicks = nicksId;
-            }
-        });
+					if($scope.pmSubmitMessage === '') {
+						//skip empty text
+					}
+					else {
+					}
+					
 
+					
 
-        $scope.sendMSG = function() {
-            if ($scope.submitMessage === '') {
-                //skip empty text
-            } else {
-                socket.emit('sendmsg', {
-                    roomName: $scope.roomId,
-                    msg: $scope.submitMessage
-                });
-            }
-
-
-            $scope.submitMessage = '';
-            var scrollToBottom = $('#content');
-            scrollToBottom.animate({
-                scrollTop: scrollToBottom.prop('scrollHeight')
-            }, 1000);
-        };
-
-        $scope.sendPmMSG = function() {
-
-            if ($scope.pmSubmitMessage === '') {
-                //skip empty text
-            } else {}
-
-
-
-
-        };
-
-
-
-
-
-
-        $scope.partRoom = function() {
-
-            socket.emit('partroom', $scope.roomId);
-
-            $location.path('/rooms/' + $scope.nickId);
-
-        };
-
-        socket.on('updatechat', function(roomId, msgHistory) {
-            if (roomId === $scope.roomId) {
-                $scope.messages = msgHistory;
-
-            }
-        });
-
-        socket.on('updatetopic', function(roomId, topic) {
-            if (roomId === $scope.roomId) {
-                $scope.topic = topic;
-
-            }
-
-        });
-
-        $scope.showPmBox = function(nick) {
-
-            if ($scope.nickSelected != nick) {
-                $scope.nickSelected = nick;
-                $scope.userSelected = true;
-            } else {
-
-                $scope.userSelected = false;
-                $scope.nickSelected = '';
-            }
-
-
-        };
+				};
+			
 
 
 
 
 
+	$scope.partRoom = function() {
 
-    }
-]);
+		socket.emit('partroom', $scope.roomId);
+
+			$location.path('/rooms/' + $scope.nickId);
+
+	};
+
+	socket.on('updatechat', function (roomId, msgHistory) {
+		if(roomId === $scope.roomId) {
+			$scope.messages = msgHistory;
+			
+		}
+	});
+
+	socket.on('updatetopic', function (roomId, topic) {
+		if(roomId === $scope.roomId) {
+			$scope.topic = topic;
+
+		}
+
+	});
+
+		$scope.showPmBox = function (nick) {
+
+			if($scope.nickSelected != nick) {
+				$scope.nickSelected = nick;
+				$scope.userSelected = true;
+			}
+
+			else {
+
+				$scope.userSelected = false;
+				$scope.nickSelected = '';
+			}
+
+		
+		};
+       	
+
+
+
+
+	
+}]);
